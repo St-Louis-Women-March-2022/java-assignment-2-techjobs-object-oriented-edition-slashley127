c0d0a0a0f3fdbe6ad1336ebefe5d49eebba4bca7
unknown <slashley127@gmail.com>
2022-09-17 10:35:21 -0500

    Test Empty Constructor

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,8 +1,13 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.assertNotEquals;
+
 
 /**
  * Created by LaunchCode
@@ -10,7 +15,13 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+
     @Test
+    public void testSettingJobId(){
+        Job jobTest1 = new Job();
+        Job jobTest2 = new Job();
+        assertNotEquals(jobTest1.getId(), jobTest2.getId());
+    }
 
 
 }
