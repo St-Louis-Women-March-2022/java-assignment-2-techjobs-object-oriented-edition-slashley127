9f300e447efb4f6aaab0b990b68b775c7878ff8a
unknown <slashley127@gmail.com>
2022-09-28 15:24:08 -0500

    deleted commented out code from Location, CoreCompetency, Employer and PostionType classes

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -8,57 +8,5 @@ public class CoreCompetency extends JobField {
         super(value);
     }
 
-//    private int id;
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    private static int nextId = 1;
-//    private String value;
-//
-//    public String getValue() {
-//        return value;
-//    }
-//
-//    public void setValue(String value) {
-//        this.value = value;
-//    }
-//
-//    public CoreCompetency() {
-//        this.id = nextId;
-//        nextId++;
-//    }
-//
-//    public CoreCompetency(String value) {
-//        this();
-//        this.value = value;
-//    }
-
-    // Custom toString, equals, and hashCode methods:
-
-//    @Override
-//    public String toString() {
-//        if(this.value == ""){
-//            return "Data not available";
-//        }
-//        return value;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (!(o instanceof CoreCompetency)) return false;
-//        CoreCompetency that = (CoreCompetency) o;
-//        return id == that.id;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id);
-//    }
-
-    // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
-    //  ONLY a getter for the 'id' field.
 
 }
